b3741c12389d712bd6dc28e2580c6668058b9565
bravojack <bravojack.contact@gmail.com>
2021-11-05 14:07:14 +0100

    Jacks edit

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,6 @@ package application;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hej");
+        System.out.println("Hej, Jack är bäst");
     }
 }
