f3a656087d67e41493165a127c60e7389b5819cb
Marko <marko@nodehill.com>
2021-11-05 14:03:07 +0100

    Added Voff Vofff

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,13 @@
 package application;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hej");
+        System.out.println("What is your Name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Cool name "+name);
     }
 }
